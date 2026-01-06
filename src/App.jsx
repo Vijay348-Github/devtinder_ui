2c5897f5bac640d874741964066a09257d0a9573
Vijay348-Github <vijayramesh4321@gmail.com>
2026-01-06 14:56:53 +0530

    Added basic routing, set up the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import NavBar from "./assets/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Home from "./Home";
+import Profile from "./Profile";
 
 function App() {
     return (
         <>
-            <NavBar />
+            <BrowserRouter basename="/">
+                <Routes>
+                    <Route path="/" element={<Body />}>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
