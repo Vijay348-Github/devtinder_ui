0f9cd9f124fa31b26b97388f01bb415a719e5b0a
Vijay348-Github <vijayramesh4321@gmail.com>
2026-01-07 16:30:21 +0530

    redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Body from "./Body";
-import Home from "./Home";
-import Profile from "./Profile";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import store from "./utils/store.jsx";
 
 function App() {
     return (
         <>
-            <BrowserRouter basename="/">
-                <Routes>
-                    <Route path="/" element={<Body />}>
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/profile" element={<Profile />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter basename="/">
+                    <Routes>
+                        <Route path="/" element={<Body />}>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/profile" element={<Profile />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
         </>
     );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Home = () => {
+    const [email, setEmail] = useState("ellyse@gmail.com");
+    const [password, setPassword] = useState("Ellyse@123");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const resultData = await axios.post(
+                BASE_URL+ "/login",
+                {
+                    email,
+                    password,
+                },
+                { withCredentials: true }
+            );
+            dispatch(addUser(resultData.data));
+            navigate("/");
+        } catch (error) {
+            console.error("Login failed:", error);
+        }
+    };
+
+    return (
+        <div className="flex justify-center my-20">
+            <div className="card bg-base-100 w-96 shadow-sm bg-cyan-300 ">
+                <div className="card-body">
+                    <h2 className="card-title justify-center">Login</h2>
+                    <div className="form-control w-full max-w-xs p-4">
+                        <label className="label">
+                            <span className="label-text font-semibold text-gray-700 pb-2">
+                                Email
+                            </span>
+                        </label>
+
+                        <input
+                            type="email"
+                            className="input input-bordered w-full"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-control w-full max-w-xs p-4">
+                        <label className="label">
+                            <span className="label-text font-semibold text-gray-700 pb-2">
+                                Password
+                            </span>
+                        </label>
+
+                        <input
+                            type="password"
+                            className="input input-bordered w-full"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="card-actions justify-center mt-4">
+                        <button
+                            className="btn btn-primary"
+                            onClick={handleLogin}
+                        >
+                            Login
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
